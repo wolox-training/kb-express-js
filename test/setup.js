37efcb0d8458f191e4b4cbfb43854cc45dac16b8
KEVIN BERMUDEZ <kevinbermudez@MacBook-Pro-de-WOLOX.local>
2021-05-04 17:16:26 -0500

    correct setup for tests

--- test/setup.js
+++ test/setup.js
@@ -2,9 +2,9 @@ const models = require('../app/models');
 
 const tables = Object.values(models.sequelize.models);
 
-const truncateTable = model => {
+const truncateTable = model =>
   model.destroy({ truncate: true, cascade: true, force: true, restartIdentity: true });
-};
+
 const truncateDatabase = () => Promise.all(tables.map(truncateTable));
 
 global.beforeEach(async () => {
