512b0fa6e79c215d6d7afbd60b735e6f75739a6d
KEVIN BERMUDEZ <kevinbermudez@MacBook-Pro-de-WOLOX.local>
2021-05-04 17:24:20 -0500

    associate model to table name and add timestamps to migration

--- app/models/User.js
+++ app/models/User.js
@@ -1,10 +1,16 @@
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-    name: { type: DataTypes.STRING, allowNull: false },
-    lastName: { type: DataTypes.STRING, allowNull: false, field: 'last_name' },
-    email: { type: DataTypes.STRING, allowNull: false, unique: true },
-    password: { type: DataTypes.STRING, allowNull: false }
-  });
+  const User = sequelize.define(
+    'User',
+    {
+      id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+      name: { type: DataTypes.STRING, allowNull: false },
+      lastName: { type: DataTypes.STRING, allowNull: false, field: 'last_name' },
+      email: { type: DataTypes.STRING, allowNull: false, unique: true },
+      password: { type: DataTypes.STRING, allowNull: false }
+    },
+    {
+      tableName: 'users'
+    }
+  );
   return User;
 };
