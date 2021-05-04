f0b9feb9f0515e425c2157ad7d8a59d0443e183c
KEVIN BERMUDEZ <kevinbermudez@MacBook-Pro-de-WOLOX.local>
2021-05-04 17:12:58 -0500

    remove innecesary console

--- test/setup.js
+++ test/setup.js
@@ -3,7 +3,6 @@ const models = require('../app/models');
 const tables = Object.values(models.sequelize.models);
 
 const truncateTable = model => {
-  console.log('holi');
   model.destroy({ truncate: true, cascade: true, force: true, restartIdentity: true });
 };
 const truncateDatabase = () => Promise.all(tables.map(truncateTable));
