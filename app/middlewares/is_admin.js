ff2f282ff1da685eec0c0a3254925b6b66b80b3c
KEVIN BERMUDEZ <kevinbermudez@MacBook-Pro-de-WOLOX.local>
2021-05-13 13:03:34 -0500

    remove try catch to is_admin middleware

--- app/middlewares/is_admin.js
+++ app/middlewares/is_admin.js
@@ -2,13 +2,9 @@ const { forbiddenError } = require('../errors');
 const { roles } = require('../../config/constants');
 
 module.exports = (req, res, next) => {
-  try {
-    if (req.authUser.role === roles.ADMIN) {
-      return next();
-    }
-
-    return next(forbiddenError('Access is only for admin'));
-  } catch (err) {
-    return next(forbiddenError('Access is only for admin'));
+  if (req.authUser.role === roles.ADMIN) {
+    return next();
   }
+
+  return next(forbiddenError('Access is only for admin'));
 };
