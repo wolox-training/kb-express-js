f3291ef9c59e591b4b2d00bf45e4e26ad3de64d2
KEVIN BERMUDEZ <kevinbermudez@MacBook-Pro-de-WOLOX.local>
2021-05-14 12:33:24 -0500

    add max length for weets as constant

--- app/controllers/weets.js
+++ app/controllers/weets.js
@@ -2,10 +2,11 @@ const { createWeet: createWeetService } = require('../services/weets');
 const { getRandomQuote } = require('../services/random_quotes');
 const { weet: weetSerializer } = require('../serializers/weets');
 const logger = require('../logger');
+const { maxLength } = require('../../config/constants').weets;
 
 exports.create = async (req, res, next) => {
   try {
-    const randomWeetContent = await getRandomQuote(140);
+    const randomWeetContent = await getRandomQuote(maxLength);
     const weetResult = await createWeetService({ content: randomWeetContent, userId: req.authUser.id });
 
     logger.info(`Weet created ${weetResult.id}`);
