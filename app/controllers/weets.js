7b060e9ef33ebf3edf0be5a6f4fcd1e627e5cd53
KEVIN BERMUDEZ <kevinbermudez@MacBook-Pro-de-WOLOX.local>
2021-05-14 12:10:59 -0500

    create route and service for get weets list

--- app/controllers/weets.js
+++ app/controllers/weets.js
@@ -1,4 +1,5 @@
-const { createWeet: createWeetService } = require('../services/weets');
+const { createWeet: createWeetService, getWeets } = require('../services/weets');
+const mapperPagination = require('../mappers/pagination');
 const { getRandomQuote } = require('../services/random_quotes');
 const { weet: weetSerializer } = require('../serializers/weets');
 const logger = require('../logger');
@@ -10,6 +11,18 @@ exports.create = async (req, res, next) => {
 
     logger.info(`Weet created ${weetResult.id}`);
     return res.status(201).send(weetSerializer(weetResult));
+  } catch (error) {
+    return next(error);
+  }
+};
+
+exports.getWeets = async (req, res, next) => {
+  try {
+    const pagination = mapperPagination(req.query.current_page, req.query.per_page);
+    const { count, weets } = await getWeets(pagination);
+    const weetsResponse = weets.map(weet => weetSerializer(weet));
+
+    return await res.status(200).send({ weets: weetsResponse, all_items: count });
   } catch (error) {
     return next(error);
   }
